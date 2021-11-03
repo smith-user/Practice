03926d107feb6d48815aabf878ee1a936463346d
andrey-krav <andrey2003030@gmail.com>
2021-11-03 22:07:29 +0500

    string

---
+++ string/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+int task7() {
+    string input, substr, newSubstr;
+    cout << "Исходная строка: ";
+    cin >> input;
+    cout << "Подстрока, которую нужно заменить: ";
+    cin >> substr;
+    cout << "Новая подстрока: ";
+    cin >> newSubstr;
+    string result;
+    int j;
+    int i = 0;
+    while(i <= input.length() - substr.length())
+    {
+        j = 0;
+        while(j < substr.length() && input[i + j] == substr[j])
+            j++;
+        if (j == substr.length())
+        {
+            result += newSubstr;
+            i += substr.length();
+        }
+        else
+        {
+            result += input[i];
+            i++;
+        }
+    }
+    cout << "Полученная строка:" << endl;
+    cout << result << endl;
+    return 0;
+}
+
+int task13() {
+    string input1, input2;
+    cout << "Первая строка: ";
+    cin >> input1;
+    cout << "Вторая строка: ";
+    cin >> input2;
+    string result;
+    for (char c : input1)
+        if (result.find(c) == -1 && input2.find(c) == -1)
+            result += c;
+    for (char c : input2)
+        if (result.find(c) == -1 && input1.find(c) == -1)
+            result += c;
+    cout << "Полученная строка:" << endl;
+    cout << result << endl;
+    return 0;
+}
+
+int main() {
+    task7();
+    //task13();
+    return 0;
+}
